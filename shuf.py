a9431095ae87a798d9e54d3761047ab3d26d4e3f
guohaiyang <guohaiyang1991@live.com>
2018-09-25 01:42:50 +0800

    洗牌机

---
+++ shuf.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# 数组移动
+
+__author__ = "slender"
+
+
+a = ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S11','S12','S13','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12','H13','C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','C13','D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','D13','J1','J2']
+
+n = int(input())
+
+indx = list(map(int, input().split()))
+
+
+for i in range(n):
+    b = a.copy()
+    for k in range(54):
+        b[indx[k]-1]= a[k]
+    a = b.copy()
+    
+    
+print(' '.join(a))
